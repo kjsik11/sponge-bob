dc1eed83207d65130487da51685141c1532f35d1
jongsik <kjsik11@gmail.com>
2023-07-01 18:09:18 +0900

    add krabby patty font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,23 @@
 import '@/styles/global.css';
 
 import { Inter } from '@next/font/google';
+import clsx from 'clsx';
 import { Metadata } from 'next';
+import localFont from 'next/font/local';
 
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
+});
+
+const krabbyPatty = localFont({
+  src: [
+    {
+      path: './fonts/KrabbyPatty.ttf',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-krabbyPatty',
 });
 
 export const metadata: Metadata = {
@@ -15,13 +27,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={clsx(inter.variable, krabbyPatty.variable)}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={`${inter.variable} font-sans`}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['var(--font-inter)', ...fontFamily.sans],
+        krabbyPatty: ['var(--font-krabbyPatty)'],
       },
     },
   },
