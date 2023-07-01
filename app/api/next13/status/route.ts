9c760e028148ffead98852bba2077907c5515062
jongsik <kjsik11@gmail.com>
2023-07-01 19:26:50 +0900

    remove unused api route file

--- app/api/next13/status/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  //TODO: consider about API wrapper functions for next13
-  return NextResponse.json({ status: 'ok' });
-}
