ee52d73c384efe8e3e73248ba853978695723839
jongsik <kjsik11@gmail.com>
2023-07-01 18:51:48 +0900

    make home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import { Button } from '@/components';
+import Generator from '@/components/custom/Generator';
 
-export default function Home() {
+import textShadowBuilder from '@/utils/text-shadow-builder';
+
+export default function HomePage() {
   return (
-    <main>
-      <p className="text-blue-500">main page</p>
-
-      <Button>button</Button>
+    <main className="px-2">
+      <h1
+        className="flex justify-center mt-8 font-spongeBoy text-4xl text-center leading-[1.5] text-amber-300"
+        style={{
+          textShadow: textShadowBuilder('#fff360'),
+        }}
+      >
+        Sponge Bob
+        <br className="md:hidden" /> Time Card Generator
+      </h1>
+      <Generator />
     </main>
   );
 }
